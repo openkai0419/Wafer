ee91b6127a0c13ec12d869da713af80eaaa39f80
okab <k020g1442.okutsu@gmail.com>
2025-08-09 18:22:56 +0900

    for new codex

--- source/constants.py
+++ source/constants.py
@@ -1,5 +1,5 @@
 
 default_db_name = "default"
-APP_FILE_NAME = "AfterImages"
-APP_NAME = "AfterImages"
-APP_ID = "nop.imageviewer.afterimages"
+APP_FILE_NAME = "ImageWafer"
+APP_NAME = "Wafer"
+APP_ID = "nop.imageviewer.wafer"

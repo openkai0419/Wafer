fbfdce492c6b105122df39d0e48345a7b0a26720
openkai0419 <openkai_0419@yahoo.co.jp>
2025-08-12 04:25:38 +0900

    Fix duplicate import

---
+++ source/common/signal.py
@@ -0,0 +1,11 @@
+class Signal:
+    """Minimal replacement for Qt Signal"""
+    def __init__(self):
+        self._callbacks = []
+
+    def connect(self, callback):
+        self._callbacks.append(callback)
+
+    def emit(self, *args, **kwargs):
+        for callback in list(self._callbacks):
+            callback(*args, **kwargs)
